78f6e3ae9736df971c72c76c39ec11a63eb7f315
Corey Becker <beckercn1@s.dcsdk12.org>
2020-09-08 11:01:18 -0600

    compleated value of a quadradic

---
+++ ch_nine/valOfQuadradic.java
@@ -0,0 +1,8 @@
+public class valOfQuadradic{
+    public static void main(String[] args) {
+        double x = 0.5;
+        double answer = 3*(x*x) - 8*(x) + 4;
+        System.out.println("Value of 3x^2-8x + 4 when x = " + x + " is " + answer);
+        
+    }
+}
