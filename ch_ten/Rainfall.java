367fde87cb64e6366ade78767ead7895ef21157c
Corey Becker <beckercn1@s.dcsdk12.org>
2020-09-09 10:57:42 -0600

    Creates chapter ten folder and rainfall

---
+++ ch_ten/Rainfall.java
@@ -0,0 +1,14 @@
+public class Rainfall {
+    public static void main(String[] args) {
+        double april = 12.0;
+        double may = 14.0;
+        double june = 8.0;
+        double avg = (april + may + june) / 3.0;
+
+        System.out.println("Rainfall for april: \t" + april);
+        System.out.println("Rainfall for may: \t"  + may);
+        System.out.println("Rainfall for june: \t" + june);
+        System.out.println("average rainfall: \t" + avg);
+
+    }
+}
