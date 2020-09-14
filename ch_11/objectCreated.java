b8c6819e14633837b57538123cf8732541f6bfc6
Corey Becker <beckercn1@s.dcsdk12.org>
2020-09-14 14:46:11 -0600

    create chapter 11 folder and the first exercise

---
+++ ch_11/objectCreated.java
@@ -0,0 +1,10 @@
+public class objectCreated{
+    public static void main(String[] args) {
+        String str = new String("Hello World");
+        String str2 = str.substring(6);
+
+        System.out.println(str);
+        System.out.println(str2);
+
+    }
+}
