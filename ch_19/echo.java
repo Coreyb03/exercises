e8491aec44fce845b91b3e4dfd86a8b4e721663e
Corey Becker <beckercn1@s.dcsdk12.org>
2020-09-28 18:06:03 -0600

    echos a word as many times as its length

--- ch_19/echo.java
+++ ch_19/echo.java
@@ -7,7 +7,7 @@ public class echo{
         System.out.println("enter a word:");
         String word = scanner.next();
         int times = word.length();
-        System.out.print("\n");
+        System.out.println();
         int i =0;
         while (i < times){
             System.out.println(word);
