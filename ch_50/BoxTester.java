78b33bf21f161bbce4fb8d896440abe94371c408
Corey Becker <beckercn1@s.dcsdk12.org>
2020-11-03 11:21:46 -0700

    creates a box where all variables are private

---
+++ ch_50/BoxTester.java
@@ -0,0 +1,13 @@
+public class BoxTester
+{
+
+  public static void main ( String[] args )
+  {
+    ImmutableBox box = new ImmutableBox( 2.5, 5.0, 6.0 ) ;
+
+    System.out.println( "Area: "  + box.area() + " volume: " + box. volume() );
+
+    System.out.println( "length: " + box.length() + " height: " + box.height() +  " width:  " + box.width() )  ;
+
+  }
+}
---
+++ ch_50/ImmutableBox.java
@@ -0,0 +1,30 @@
+public class ImmutableBox {
+    private double width;
+    private double height;
+    private double length;
+
+    public ImmutableBox(double width, double height, double length){
+        this.width = width;
+        this.height = height;
+        this.length = length;
+    }
+
+    public double volume(){
+        return width * height * length;
+    }
+    public double area(){
+        return 2*(width * length + height * length + height * width);
+    }
+    public double length(){
+        return length;
+    } 
+    public double height(){
+        return height;
+    }
+    public double width(){
+        return width;
+    }
+
+
+
+}
